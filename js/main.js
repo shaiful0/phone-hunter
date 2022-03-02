016982ff7a13855938d62bff7b08cc5d8252ee6c
sharifulislam <shariful0401@gmail.com>
2022-03-02 20:43:19 +0600

    added js

--- js/main.js
+++ js/main.js
@@ -41,9 +41,55 @@ const displayResult = (result) => {
   }
   const seeMore = document.getElementById('see-more')
   // seemore button
-  seeMore.onclick = () => {
-      //sliced variable
-      sliced = result.data
-      // clearing contents
-      document.getElementById('search-results').textContent = ''
-      document.getElementById('display-details').textContent = ''
+seeMore.onclick = () => {
+//sliced variable
+sliced = result.data
+ // clearing contents
+ document.getElementById('search-results').textContent = ''
+document.getElementById('display-details').textContent = ''
+  // showing card
+  sliced.forEach(item => {
+ const sectionContainer = document.getElementById('search-results')
+  const div = document.createElement('div')
+ div.classList.add('col')
+  div.innerHTML = `
+  <div class="card h-100 bg-white">
+      <img src="${item.image}" class="card-img-top" alt="...">
+      <div class="card-body d-flex flex-column justify-content-between">
+          <h4 class="card-title fw-bold">Name: ${item.phone_name}</h4>
+          <h5 class="card-title fw-bold">Brand: ${item.brand}</h5>
+           <div class="d-flex justify-content-center">
+              <button class="btn btn-primary w-75" onclick="loadDetails('${item.slug}')">See Details</button>
+          </div>
+      </div>
+  </div>
+  `;
+ sectionContainer.appendChild(div)
+// show more button hiding
+ document.getElementById('see-more').style.display = 'none'
+})
+}
+  // clearing contents of textcontents 
+  document.getElementById('search-results').textContent = ''
+  document.getElementById('display-details').textContent = ''
+  // showing each card by foreach loop
+  sliced.forEach(item => {
+  const sectionContainer = document.getElementById('search-results')
+  const div = document.createElement('div')
+  div.classList.add('col')
+  div.innerHTML = `
+    <div class="card h-100 bg-white">
+        <img src="${item.image}" class="card-img-top" alt="...">
+        <div class="card-body d-flex flex-column justify-content-between">
+            <h4 class="card-title fw-bold">Name: ${item.phone_name}</h4>
+            <h5 class="card-title fw-bold">Brand: ${item.brand}</h5>
+            <div class="d-flex justify-content-center">
+                <button class="btn btn-primary w-75" onclick="loadDetails('${item.slug}')">See Details</button>
+            </div>
+        </div>
+    </div>
+`;
+     
+      sectionContainer.appendChild(div)
+  })
+}
