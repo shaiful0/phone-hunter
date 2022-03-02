b0ebafbfd9016b87a18f86fe067336ac2cd57c15
sharifulislam <shariful0401@gmail.com>
2022-03-02 20:38:06 +0600

    add js

--- js/main.js
+++ js/main.js
@@ -17,3 +17,33 @@ const loadSearchResult = () => {
       errorDisplay('block')
   }
 }
+
+//function for error message
+const errorDisplay = currentStatus => {
+  document.getElementById('error-msg').style.display = currentStatus
+}
+// display result 
+const displayResult = (result) => {
+  // removing error message 
+  errorDisplay('none')
+  // checking if input keywords 
+  if (result.status == false) {
+      // showing error msg
+      errorDisplay('block')
+  }
+  let sliced = result.data
+  document.getElementById('see-more').style.display = 'none'
+  
+  if (result.data.length > 20) {
+      // slicing items 
+      sliced = result.data.slice(0, 20)
+      document.getElementById('see-more').style.display = 'block'
+  }
+  const seeMore = document.getElementById('see-more')
+  // seemore button
+  seeMore.onclick = () => {
+      //sliced variable
+      sliced = result.data
+      // clearing contents
+      document.getElementById('search-results').textContent = ''
+      document.getElementById('display-details').textContent = ''
